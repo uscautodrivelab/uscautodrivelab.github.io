732483736f3db3afd1e1a8095b3e6246d52940fe
Matthew Schulz <74387737+mkschulz9@users.noreply.github.com>
2024-06-13 10:36:28 -0700

    feat: add drawer when opening menu

--- src/components/navigation-bar/NavigationBar.tsx
+++ src/components/navigation-bar/NavigationBar.tsx
@@ -4,29 +4,52 @@ import {
   Toolbar,
   Typography,
   IconButton,
-  Menu,
-  MenuItem,
+  Drawer,
   Box,
   Button,
+  ButtonBase,
+  List,
+  ListItem,
+  ListItemText,
 } from '@mui/material';
 import MenuIcon from '@mui/icons-material/Menu';
 import usc_logo from '../../assets/usc_logo.png';
 
 export const NavigationBar: React.FC = () => {
   const pages = ['Research Digest', '[Blank]', '[Blank]'];
-  const [anchorElNav, setAnchorElNav] = React.useState<null | HTMLElement>(
-    null
-  );
+  const [drawerOpen, setDrawerOpen] = React.useState(false);
 
-  const handleOpenNavMenu = (event: React.MouseEvent<HTMLElement>) => {
-    setAnchorElNav(event.currentTarget);
-  };
-  const handleCloseNavMenu = () => {
-    setAnchorElNav(null);
-  };
+  const toggleDrawer =
+    (open: boolean) => (event: React.KeyboardEvent | React.MouseEvent) => {
+      if (
+        event.type === 'keydown' &&
+        ((event as React.KeyboardEvent).key === 'Tab' ||
+          (event as React.KeyboardEvent).key === 'Shift')
+      ) {
+        return;
+      }
+      setDrawerOpen(open);
+    };
+
   const handleClick = () => {
-    console.log('Button Clicked (Long View)');
+    console.log('Button Clicked.');
   };
+
+  const listMenuOptions = () => (
+    <Box
+      sx={{ width: 'auto' }}
+      onClick={toggleDrawer(false)}
+      onKeyDown={toggleDrawer(false)}
+    >
+      <List>
+        {pages.map(text => (
+          <ListItem button key={text} onClick={handleClick}>
+            <ListItemText primary={text} />
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
 
   return (
     <AppBar position="static">
@@ -36,48 +59,31 @@ export const NavigationBar: React.FC = () => {
           justifyContent: 'space-between',
         }}
       >
-        <Box
+        <ButtonBase
+          onClick={handleClick}
           sx={{
             display: 'flex',
             alignItems: 'center',
+            justifyContent: 'center',
+            padding: 0.5,
+            borderRadius: '4px',
           }}
         >
           <img src={usc_logo} style={{ height: 30, marginRight: 10 }} />
           <Typography variant="h5">AutoDrive Lab</Typography>
-        </Box>
-
+        </ButtonBase>
         <Box sx={{ display: { xs: 'flex', md: 'none' } }}>
           <IconButton
             size="small"
-            onClick={handleOpenNavMenu}
+            onClick={toggleDrawer(true)}
             color="inherit"
             sx={{ mr: 2 }}
           >
             <MenuIcon />
           </IconButton>
-          <Menu
-            anchorEl={anchorElNav}
-            anchorOrigin={{
-              vertical: 'bottom',
-              horizontal: 'left',
-            }}
-            keepMounted
-            transformOrigin={{
-              vertical: 'top',
-              horizontal: 'left',
-            }}
-            open={Boolean(anchorElNav)}
-            onClose={handleCloseNavMenu}
-            sx={{
-              display: { xs: 'block', md: 'none' },
-            }}
-          >
-            {pages.map(page => (
-              <MenuItem key={page} onClick={handleClick}>
-                <Typography>{page}</Typography>
-              </MenuItem>
-            ))}
-          </Menu>
+          <Drawer anchor="top" open={drawerOpen} onClose={toggleDrawer(false)}>
+            {listMenuOptions()}
+          </Drawer>
         </Box>
         <Box
           sx={{
@@ -86,7 +92,11 @@ export const NavigationBar: React.FC = () => {
           }}
         >
           {pages.map(page => (
-            <Button key={page} onClick={handleClick} sx={{ color: 'white' }}>
+            <Button
+              key={page}
+              onClick={handleClick}
+              sx={{ color: 'white', marginRight: 2 }}
+            >
               {page}
             </Button>
           ))}
